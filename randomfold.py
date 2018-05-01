c2b198e78d2de2e4e24915e05b0703e3dd22a94f
stanrozing <31852910+stanrozing@users.noreply.github.com>
2018-05-01 12:34:14 +0200

    Update randomfold.py

--- randomfold.py
+++ randomfold.py
@@ -8,54 +8,33 @@ def RandomFold(protein):
     # instantiate variables
     directions = []
     last_coordinate = [2*len(protein.sequence) + 3, 2*len(protein.sequence) + 3]
-    coordinates = [last_coordinate]
-    i = 0
+    start = True
 
     # for each amino acid in the protein, randomly go into a direction
-    for amino in protein.sequence:
-        directions = []
+    for i in range(len(protein.sequence)):
 
-        # check which directions are free to go to
-        if protein.grit.grit[last_coordinate[0] + 2][last_coordinate[1]] == ' ' :
-            directions.append([2,0])
-
-        if protein.grit.grit[last_coordinate[0] - 2][last_coordinate[1]] == ' ' :
-            directions.append([-2,0])
-
-        if protein.grit.grit[last_coordinate[0]][last_coordinate[1] + 2] == ' ' :
-            directions.append([0,2])
-
-        if protein.grit.grit[last_coordinate[0]][last_coordinate[1] - 2] == ' ' :
-            directions.append([0,-2])
+        # looks which directions are available
+        directions = protein.grit.is_valid(last_coordinate[0], last_coordinate[1])
 
         # if there are free directions, pick one and place the amino acid there
         if len(directions) > 0 :
 
             # place the amino acid on the correct coordinates
             direction = directions[randint(0, len(directions) - 1)]
-            amino[1] = [last_coordinate[0] + direction[0], last_coordinate[1] + direction[1]]
-            protein.grit.grit[amino[1][0]][amino[1][1]] = amino[0]
+            protein.coordinates[i] = [last_coordinate[0] + direction[0], last_coordinate[1] + direction[1]]
+            protein.grit.grit[protein.coordinates[i][0]][protein.coordinates[i][1]] = protein.sequence[i]
 
-            # place the connection on the correct coordinates
-            if direction == [2,0]:
-                connection = [1,0]
-            elif direction == [-2,0]:
-                connection = [-1,0]
-            elif direction == [0,2]:
-                connection = [0,1]
-            else:
-                connection = [0,-1]
-            connection_place = [last_coordinate[0] + connection[0], last_coordinate[1] + connection[1]]
-            if i != 0 :
-                if connection[0] != 0:
-                    protein.grit.grit[connection_place[0]][connection_place[1]] = '|'
+            # place the correct connection on the correct position
+            connection = [int((last_coordinate[0] + protein.coordinates[i][0]) / 2), int((last_coordinate[1] + protein.coordinates[i][1]) / 2)]
+            if not start :
+                if connection[0] != last_coordinate[0]:
+                    protein.grit.grit[connection[0]][connection[1]] = '|'
                 else:
-                    protein.grit.grit[connection_place[0]][connection_place[1]] = '-'
-            coordinates.append(amino[1])
-            i += 1
+                    protein.grit.grit[connection[0]][connection[1]] = '-'
+            start = False
 
             # change the last coordinate
-            last_coordinate = amino[1]
+            last_coordinate = protein.coordinates[i]
 
         # otherwise, give up and return a score of 0
         else :
