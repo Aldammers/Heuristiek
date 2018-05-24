4b07471f8ae6f12a3a75c9d61866b80717d34f42
Aldammers <31691842+Aldammers@users.noreply.github.com>
2018-05-24 17:37:58 +0200

    Update MainRandom.py

--- Code/Algorithms/MainRandom.py
+++ Code/Algorithms/MainRandom.py
@@ -1,5 +1,6 @@
 import classes as c
 import RandomFold as rf
+import timeit
 
 
 # get the needed user input
@@ -8,6 +9,7 @@ n = input('Fold how many times: ')
 n = int(n)
 print()
 
+start = timeit.default_timer()
 # fold the given protein n times
 myProtein = c.Protein(protein_string)
 startingpoint = [2*len(myProtein.sequence) + 3, 2*len(myProtein.sequence) + 3]
@@ -16,3 +18,4 @@ results = rf.Random_n(myProtein, n, startingpoint)
 results[0].grit.reveal()
 print('Score: ', results[1])
 print("This is the folding with the best score of the", n, " we tried!")
+print("Runtime", stop - start, "seconds")
