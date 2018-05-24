7040a2f51b26d973d7401360eca6355b44e27817
Aldammers <31691842+Aldammers@users.noreply.github.com>
2018-05-24 17:40:02 +0200

    Update MainBreadth.py

--- Code/Algorithms/MainBreadth.py
+++ Code/Algorithms/MainBreadth.py
@@ -1,8 +1,11 @@
 from classes import *
 from BreadthFirst import *
+import timeit
 
 proteinsequence = choose()
 print(proteinsequence)
+
+start = timeit.default_timer()
 foldings = breadth(len(proteinsequence))
 
 proteins = convert_queue(proteinsequence, foldings)
@@ -11,3 +14,6 @@ result = found_score(proteins)
 print(result[0])
 for protein in result[1]:
     protein.grit.reveal()
+
+stop = timeit.default_timer()
+print("Runtime:", stop - start, "seconds")
