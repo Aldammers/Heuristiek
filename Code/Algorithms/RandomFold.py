928b6932d8cc605627cc742105f73207a7cc9951
whraa <31894131+whraa@users.noreply.github.com>
2018-05-25 21:18:57 +0200

    Update RandomFold.py

--- Code/Algorithms/RandomFold.py
+++ Code/Algorithms/RandomFold.py
@@ -1,247 +1,63 @@
-# protein class
-class Protein:
+from random import randint
+import copy
 
-    #instantiate the grit and the sequence
-    def __init__(self, sequence):
-        self.sequence = sequence
-        self.length = len(sequence)
-        self.coordinates = []
-        self.directions = []
-        self.grit = Grit(4*self.length + 9)
-        for i in range(self.length):
-            self.coordinates.append([0,i])
+# function to randomly fold a protein
+def RandomFold(protein, startingpoint):
 
+    # instantiate variables
+    directions = []
+    last_coordinate = startingpoint
+    start = True
 
-    # function to return the score of a protein when folded
-    def FoldedScore(self):
-        correction = 0
-        for i in range(self.length - 1):
-            if self.sequence[i][0] == 'H':
-                if self.sequence[i + 1][0] == 'H' :
-                    correction += 1
-                if self.sequence[i + 1][0] == 'C':
-                    correction += 1
-            if self.sequence[i][0] == 'C':
-                if self.sequence[i + 1][0] == 'H' :
-                    correction += 1
-                if self.sequence[i + 1][0] == 'C':
-                    correction += 5
-        return correction - self.grit.score()
+    # for each amino acid in the protein, randomly go into a direction
+    for i in range(len(protein.sequence)):
 
+        # looks which directions are available
+        directions = protein.grit.isValid(last_coordinate[0], last_coordinate[1])
 
-    # fill in the amino acids at the right coordinates
-    def fillGrit(self):
+        # if there are free directions, pick one and place the amino acid there
+        if len(directions) > 0 :
 
-        self.grit.reset()
-        cursor = [self.grit.size // 2, self.grit.size // 2]
+            # place the amino acid on the correct coordinates
+            direction = directions[randint(0, len(directions) - 1)]
+            protein.coordinates[i] = [last_coordinate[0] + direction[0], last_coordinate[1] + direction[1]]
+            protein.grit.grit[protein.coordinates[i][0]][protein.coordinates[i][1]] = protein.sequence[i]
 
-        for i in range(self.length - 1):
+            # place the correct connection on the correct position
+            connection = [int((last_coordinate[0] + protein.coordinates[i][0]) / 2), int((last_coordinate[1] + protein.coordinates[i][1]) / 2)]
+            if not start :
+                if connection[0] != last_coordinate[0]:
+                    protein.grit.grit[connection[0]][connection[1]] = '|'
+                else:
+                    protein.grit.grit[connection[0]][connection[1]] = '-'
+            start = False
 
-            self.grit.grit[cursor[0]][cursor[1]] = self.sequence[i]
-            if self.directions[i][0] == 0:
-                self.grit.grit[cursor[0] + self.directions[i][0]][cursor[1]  + self.directions[i][1]] = '-'
-            else:
-                self.grit.grit[cursor[0] + self.directions[i][0]][cursor[1]  + self.directions[i][1]] = '|'
+            # change the last coordinate
+            last_coordinate = protein.coordinates[i]
 
-            cursor[0] = cursor[0] + 2 * self.directions[i][0]
-            cursor[1] = cursor[1] + 2 * self.directions[i][1]
-        j = self.length - 1
-        self.grit.grit[cursor[0]][cursor[1]] = self.sequence[j]
-        
-    def coordinatesToDirections(self):
-        for i in range(self.length - 1):
-            direction1 = (self.coordinates[i+1][0] - self.coordinates[i][0]) // 2
-            direction0 = (self.coordinates[i+1][1] - self.coordinates[i][1]) // 2
-            direction = [direction0, direction1]
-            self.directions.append(direction)
+        # otherwise, give up and return a score of 0
+        else :
+            return 0.0
 
-    # calculate the score, ireating over the folded protein
-    def ezScore(self):
-        score = 0
+    # return the score of the folded protein
+    return protein.FoldedScore()
 
-        def invert(direction):
-            new_direction = [0,0]
-            new_direction[0] = - 2 * direction[0]
-            new_direction[1] = - 2 * direction[1]
-            return new_direction
+# function to randomly fold a protein n times
+def RandomN(protein, n, startingpoint):
 
-        def normalise(direction):
-            new_direction = [0,0]
-            new_direction[0] = 2 * direction[0]
-            new_direction[1] = 2 * direction[1]
-            return new_direction
+    # instantiate variables
+    best_protein = protein
+    best_score = 0
 
-        surrounding = [[0,2],[2,0],[0,-2],[-2,0]]
-        cursor = [self.grit.size // 2, self.grit.size // 2]
+    # fold n times
+    for i in range(n):
+        protein.grit.reset()
+        score = RandomFold(protein, startingpoint)
 
-        if self.sequence[0] == 'H':
-            for tile in surrounding:
-                if not tile == [0,2]:
-                    if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'C' or self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'H':
-                        score += 1
+        # keep the score and folding if it is the best until now
+        if score < best_score:
+            best_score = score
+            best_protein = copy.deepcopy(protein)
 
-        elif self.sequence[0] == 'C':
-            for tile in surrounding:
-                if not tile == [0,2]:
-                    if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'C':
-                        score += 5
-                    if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'H':
-                        score += 1
-
-
-        cursor[0] = cursor[0] + 2 * self.directions[0][0]
-        cursor[1] = cursor[1] + 2 * self.directions[0][1]
-
-
-        for i in range(1, self.length - 1):
-            if self.sequence[i] == 'H':
-                for tile in surrounding:
-                    if not tile in (invert(self.directions[i - 1]), normalise(self.directions[i])):
-                        if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'C' or self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'H':
-                            score += 1
-
-            elif self.sequence[i] == 'C':
-                for tile in surrounding:
-                    if not tile in (invert(self.directions[i - 1]), normalise(self.directions[i])):
-                        if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'C':
-                            score += 5
-                        if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'H':
-                            score += 1
-
-            cursor[0] = cursor[0] + 2 * self.directions[i][0]
-            cursor[1] = cursor[1] + 2 * self.directions[i][1]
-
-        if self.sequence[self.length - 1] == 'H':
-            for tile in surrounding:
-                if not tile == invert(self.directions[self.length - 2]):
-                    if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'C' or self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'H':
-                        score += 1
-
-        elif self.sequence[self.length - 1] == 'C':
-            for tile in surrounding:
-                if not tile == invert(self.directions[self.length - 2]):
-                    if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'C':
-                        score += 5
-                    if self.grit.grit[cursor[0] + tile[0]][cursor[1] + tile[1]] == 'H':
-                        score += 1
-
-        return -score//2
-
-    def segmentise(self, argument, method):
-        if method == 'default':
-            segment_size = argument
-            segments = []
-            c = 0
-            while c < len(self.sequence) - segment_size:
-                segments.append(self.sequence[c:c+segment_size])
-                c += segment_size
-            segments.append(self.sequence[c:len(self.sequence)])
-            return segments
-
-        elif method == 'manual':
-            segments = argument
-            return segments
-
-        elif method == 'cleverly':
-            segment = self.sequence[0]
-            H = self.sequence.count('H')
-            P = self.sequence.count('P')
-            segments = []
-            c = 0
-            for i in range(self.length):
-                segment = self.sequence[c:i]
-                partialH = segment.count('H')
-                partialP = segment.count('P')
-                if i - c > 7:
-                    segments.append(segment)
-                    c = i
-                elif H * partialP >= P * partialH:
-                    segments.append(segment)
-                    c = i
-            return segments
-
-
-
-# Grit class
-class Grit:
-
-    # instantiate the grit with correct size
-    def __init__(self, size):
-        self.grit = [[' ' for x in range(size)] for y in range(size)]
-        self.size = size
-
-    # function which finds the place of the protein in the grit, so that no useless part of the grit gets printed
-    def proteinPlace(self):
-        self.top = self.size
-        self.left = self.size
-        self.right = 0
-        self.bottom = 0
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.grit[i][j] != ' ':
-                    if i < self.top:
-                        self.top = i
-                    elif i > self.bottom:
-                        self.bottom = i
-                if self.grit[i][j] != ' ':
-                    if j < self.left:
-                        self.left = j
-                    elif j > self.right:
-                        self.right = j
-
-
-    # show the grit
-    def reveal(self):
-        self.proteinPlace()
-        for i in range(self.top - 1, self.bottom + 2):
-            for j in range(self.left - 1, self.right + 2):
-                print(self.grit[i][j], end=' ')
-            print()
-        print()
-
-    # reset the grit to its initial state
-    def reset(self):
-        self.grit = [[' ' for x in range(self.size)] for y in range(self.size)]
-
-    # function to determine the score of a grit (the protein on it to be precise)
-    def score(self):
-        score = 0
-        for i in range(2, self.size - 2, 2):
-            for j in range(2, self.size - 2, 2):
-
-                # check for any bonds
-                if self.grit[i][j] != 'P' and self.grit[i][j] != ' ':
-                    if self.grit[i+2][j] != 'P' and self.grit[i+2][j] != ' ':
-                        score += 0.5
-                    if self.grit[i-2][j] != 'P' and self.grit[i-2][j] != ' ':
-                        score += 0.5
-                    if self.grit[i][j+2] != 'P' and self.grit[i][j+2] != ' ':
-                        score += 0.5
-                    if self.grit[i][j-2] != 'P' and self.grit[i][j-2] != ' ':
-                        score += 0.5
-
-                # check for C-C bonds
-                if self.grit[i][j] == 'C':
-                    if self.grit[i+2][j] == 'C':
-                        score += 2
-                    if self.grit[i-2][j] == 'C':
-                        score += 2
-                    if self.grit[i][j+2] == 'C':
-                        score += 2
-                    if self.grit[i][j-2] == 'C':
-                        score += 2
-
-        return score
-
-    # determine which position surrounding an amino acid are still available
-    def isValid(self, m, n):
-        directions = []
-        if self.grit[m][n+2] == ' ':
-            directions.append([0,2])
-        if self.grit[m][n-2] == ' ':
-            directions.append([0,-2])
-        if self.grit[m-2][n] == ' ':
-            directions.append([-2,0])
-        if self.grit[m+2][n] == ' ':
-            directions.append([2,0])
-        return directions
+    # return the best folding and score
+    return(best_protein, best_score)
