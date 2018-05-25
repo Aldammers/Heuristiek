4e386791339bea6a72f0d399b9abc969fab89a02
whraa <31894131+whraa@users.noreply.github.com>
2018-05-25 22:04:30 +0200

    Update BreadthFirst.py

--- Code/Algorithms/BreadthFirst.py
+++ Code/Algorithms/BreadthFirst.py
@@ -2,6 +2,7 @@ from classes import *
 import queue
 import copy
 
+# checks the surrounding of an amino acid for available places
 def validate(directions, n):
     plain = [[0 for x in range(2*n - 1)] for y in range(2*n - 1)]
     options = []
@@ -30,7 +31,7 @@ def validate(directions, n):
         options = [[0,1],[1,0]]
     return options
 
-
+# computes all possible foldings of a protein of certain length
 def breadth(length):
 
     possibilities = queue.Queue()
@@ -57,6 +58,7 @@ def breadth(length):
 
     return possibilities
 
+# emties a queue and put its values in a list
 def convertQueue(sequence, possibilities):
     proteins = []
     while not possibilities.empty():
@@ -70,6 +72,7 @@ def convertQueue(sequence, possibilities):
 
     return proteins
 
+# iterates over proteins to return only those with the highest score
 def foundScore(proteins):
     best_score = 0
     good_protein = Protein('H')
@@ -98,6 +101,7 @@ def foundScore(proteins):
 
 # extention for piecewise method
 
+# computes all possible ways to legitimately append a segment of certain length to an existing folding
 def piecewiseBreadth(length, progressions):
 
     possibilities = queue.Queue()
@@ -125,42 +129,42 @@ def piecewiseBreadth(length, progressions):
 
     return possibilities
 
+# segment by segment concatenate all segments forming a protein
 def concatenate(concatenation, progressions, segments, initiated, sequence, n):
     if progressions == [] and not n == 0:
         print('All proceeding possibilities using this segmentation were exhausted, sawry.')
         return 'error'
     if initiated:
         if not concatenation == sequence:
-            print(n)
-            print(segments[n - 1])
+            print('one step further')
             propagation = piecewiseBreadth(len(segments[n-1]), progressions)
             proteins = convertQueue(concatenation, propagation)
             result = foundScore(proteins)
             progressions = []
             for candidate in result[1]:
                 progressions.append(candidate.directions)
-            print(progressions)
             concatenation = concatenation + segments[n]
             return concatenate(concatenation, progressions, segments, initiated, sequence, n+1)
 
         else:
-            print('done')
+            print('last step')
             propagation = piecewiseBreadth(len(segments[n-1]), progressions)
             proteins = convertQueue(concatenation, propagation)
             result = foundScore(proteins)
             return result
 
     else:
-        print('1')
         initiation = breadth(len(concatenation))
         proteins = convertQueue(concatenation, initiation)
         result = foundScore(proteins)
         for candidate in result[1]:
             progressions.append(candidate.directions)
-        print(progressions)
         initiated = True
-        if not concatenation == sequence:
-            concatenation = concatenation + segments[1]
+        if len(segments) == 1:
+            proteins = convertQueue(concatenation, initiation)
+            result = foundScore(proteins)
+            return result
+        concatenation = concatenation + segments[1]
         return concatenate(concatenation, progressions, segments, initiated, sequence, 2)
 
 
